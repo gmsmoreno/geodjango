90dd70e72b7c38fe1fc539e2470294f38f3fa6fb
gabriel <gmsmoreno@gmail.com>
2022-04-07 17:12:45 -0300

    first commit

--- geoserver-rest/gs-rest.py
+++ geoserver-rest/gs-rest.py
@@ -2,16 +2,16 @@
 from geo.Geoserver import Geoserver
 
 # Initialize the library
-geo = Geoserver('https://geoserver.merx.tech/geoserver', username='gabriel', password='Mudar@123')
+geo = Geoserver('http://localhost:8080/geoserver', username='admin', password='geoserver')
 
 # geo.delete_workspace(workspace='demo')
 
 # Import raster to geoserver
 # geo.create_coveragestore(layer_name='raster1', path=r'/home/gabriel/anaconda3/envs/geo-web/data/raster1.tif', workspace='demo')
 
-geo.create_featurestore(store_name='geo_data', workspace='geoapp', db='carbon', host='34.95.180.123', pg_user='gabriel', pg_password='Mudar@123')
+geo.create_featurestore(store_name='geo_data', workspace='geoapp', db='data', host='localhost', pg_user='postgres', pg_password='postgres')
 
-geo.publish_featurestore(workspace='geoapp', store_name='geo_data', pg_table='app_ac')
+geo.publish_featurestore(workspace='geoapp', store_name='geo_data', pg_table='C_Jamoat')
 
 # geo.upload_style(r'/home/gabriel/anaconda3/envs/geo-web/data/style/raster1.sld', workspace='demo')
 
@@ -23,4 +23,4 @@ geo.publish_featurestore(workspace='geoapp', store_name='geo_data', pg_table='ap
 
 # geo.create_outline_featurestyle('polygon-style', workspace='demo')
 
-# geo.publish_style(layer_name='app_ac', style_name='polygon-style', workspace='demo')
+# geo.publish_style(layer_name='C_Jamoat', style_name='polygon-style', workspace='demo')
